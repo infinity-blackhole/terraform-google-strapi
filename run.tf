e107f2ec32c6e1dd0ec7d59e1c000d5be3bcd0ad
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-05-23 13:28:03 +0000

    Fix ignore error

--- run.tf
+++ run.tf
@@ -51,7 +51,7 @@ resource "google_cloud_run_service" "strapi" {
   }
   lifecycle {
     ignore_changes = [
-      template.spec.containers[0].image
+      template[0].spec[0].containers[0].image
     ]
   }
   depends_on = [
