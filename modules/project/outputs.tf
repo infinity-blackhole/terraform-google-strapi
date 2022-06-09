87e80a7f361d2413b2cbd881cac08676260a1d0d
Phetsinorath William <phetsinorath.w@sfeir.com>
2022-06-09 11:58:55 +0000

    Add container registry url output

--- modules/project/outputs.tf
+++ modules/project/outputs.tf
@@ -10,3 +10,11 @@ output "network" {
   value       = google_compute_network.default
   description = "Network"
 }
+
+output "container_registry" {
+  value = join("/", [
+    "${google_artifact_registry_repository.default_container_registry.location}-docker.pkg.dev",
+    google_artifact_registry_repository.default_container_registry.project,
+    google_artifact_registry_repository.default_container_registry.repository_id
+  ])
+}
